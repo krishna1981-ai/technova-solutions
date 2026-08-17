c0a5f057299149f1a732713d7a5cfdd667911ae4
Krishna Tripathi <kt850412@gmail.com>
2026-08-17 21:18:13 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const sqlite3 = require('sqlite3').verbose();
 const session = require('express-session');
 const app = express();
 const PORT = 3000;
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(session({
